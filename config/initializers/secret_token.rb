3812f3d19b10970996c3835d7d7c5748eece0b21
mjvezzani <mjvezzani@gmail.com>
2014-01-16 06:52:44 -0500

    Create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SleepTracker::Application.config.secret_key_base = 'f9481b0be66836eb57de123ae245ebb9f9d2ef0aaa33c339ddd348b890c296b75a4e37e44ccf06782927b1372b3456a7000103632f090b50b3d45bf75da4ff17'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SleepTracker::Application.config.session_store :cookie_store, key: '_sleep_tracker_session'
